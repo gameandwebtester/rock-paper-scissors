2cba1ee6f5c795246d76c0628d74cd09223f3e10
gameandwebtester <elaskowski@hotmail.ca>
2022-10-18 20:22:35 -0400

    changed the playRound funtion to use switch statements instead of if else

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,4 @@
 let choices = ["rock", "paper", "scissors"]
-//console.log(choices[1])
-
 let playerScore = 0;
 let computerScore = 0;
 
@@ -13,55 +11,62 @@ function score() {
     playerScore +=1
 }
 
-//console.log(getComputerChoice())
-
-function playRound(playerSelection, computerSelection) {
+function getPlayerChoice(playerSelection) {
     playerSelection = prompt("enter either 'rock', 'paper', or 'scissors'").toLowerCase();
-    //playerSelection = "rock"
-    computerSelection = getComputerChoice();
     if (playerSelection === choices[0].toString() || playerSelection === choices[1].toString() || 
         playerSelection === choices[2].toString()) {
+            return playerSelection;
     }
     else {
         console.error("please type a valid object");
         return;
     }
-    if(playerSelection === computerSelection){
-        console.log("you tied with the machine")
-    }
-     else if(playerSelection === "rock" && computerSelection === "scissors") {
-        score();
-        console.log("you win, rock beats scissors")
-    }
-    else if(playerSelection === "paper" && computerSelection === "rock") {
-        score();
-        console.log("you win paper beats rock")
-    }
-    else if(playerSelection === "scissors" && computerSelection === "paper") {
-        score();
-        console.log("you win scissors beats paper")
-    }
-    else{
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = getPlayerChoice(playerSelection);
+    computerSelection = getComputerChoice();
+
+    switch (true) {
+        case playerSelection === computerSelection: 
+            console.log("you tied with the machine");
+            break;
+        case playerSelection === "rock" && computerSelection == "scissors":
+        case playerSelection === "paper" && computerSelection === "rock":
+        case playerSelection === "scissors" && computerSelection === "paper":
+            score();
+            console.log("you win " + playerSelection + " beats " + computerSelection);
+            break;
+        case playerSelection === undefined:
+            playerScore += 0;
+            computerScore += 0;
+            break;
+        default:
         computerScore += 1;
-        console.log("you lose " + computerSelection + " beats " + playerSelection)
+        console.log("you lose " + computerSelection + " beats " + playerSelection) 
+        break;
     }
     return playerSelection + ' ' + computerSelection
 }
 
-
-
 function game() {
-    for (let i = 0; i < 5; i++) {
-        if (i < 5 ) {
-            playRound();
-            console.log(i);
+    while(playerScore < 3 || computerScore < 3){
+        playRound()
+        if (playerScore == 3 || computerScore == 3) {
+            console.log("game is over");
+            break;
         }
-        else{
-            console.log("game is over")
-            return;
-        }
+    }
+    if( playerScore > computerScore) {
+        console.log("The player wins the game")
+    }
+    else if(computerScore > playerScore ){
+        console.log("the machine wins the game")
+    }
+    else{
+        console.log("the game is a tie")
     }
 }
 
-console.log(game());
-console.log(playerScore + ' is the score')
+game();
+console.log(playerScore + " for the player and " +  computerScore + " for the machine is the final score")
